f6497b616a84af7f65330ac8eeffb302706fb0cf
nguyenhieu67 <149569751+nguyenhieu67@users.noreply.github.com>
2026-01-06 16:04:18 +0700

    Correct homework 2026-1-3

--- 2026-1-3/main.js
+++ 2026-1-3/main.js
@@ -1,5 +1,5 @@
 // Task 1:
-function studentScore(num) {
+function getStudentLevel(num) {
     if (typeof num !== "number" || isNaN(num))
         return console.log("Please enter a valid number.");
     if (num < 0 || num >= 11)
@@ -18,50 +18,56 @@ function studentScore(num) {
     }
 }
 
-studentScore("9");
-studentScore(NaN);
-studentScore(-1);
-studentScore(9);
-studentScore(6.4);
+getStudentLevel("9");
+getStudentLevel(NaN);
+getStudentLevel(-1);
+getStudentLevel(9);
+getStudentLevel(6.4);
 
 // Task 2:
-function month(num) {
+function getDaysInMonth(num) {
     if (typeof num !== "number" || isNaN(num))
         return console.log(`Please ${num} a valid number.`);
 
-    switch (true) {
-        case num === 1 ||
-            num === 3 ||
-            num === 5 ||
-            num === 7 ||
-            num === 8 ||
-            num === 10 ||
-            num === 12:
+    switch (num) {
+        case 1:
+        case 3:
+        case 5:
+        case 7:
+        case 8:
+        case 10:
+        case 12:
             console.log("Have 31 days");
             break;
-        case num === 2 || num === 4 || num === 6 || num === 9 || num === 11:
+        case 4:
+        case 6:
+        case 9:
+        case 11:
             console.log("Have 30 days");
             break;
+        case 2:
+            console.log("Have 28 days");
+            break;
         default: {
-            console.log(`Please ${num} a number between 1 and 12.`);
+            console.log(`Invalid month`);
         }
     }
 }
 
-month(1);
-month(2);
-month(3);
-month(4);
+getDaysInMonth(1);
+getDaysInMonth(2);
+getDaysInMonth(3);
+getDaysInMonth(4);
 
 // Task 3:
-function evenOddCheck(n) {
+function isEvenNumber(n) {
     if (typeof n !== "number" || isNaN(n))
         return console.log("Please enter a valid number.");
     return n % 2 === 0 ? console.log("Even") : console.log("Odd");
 }
 
-evenOddCheck(10);
-evenOddCheck(31);
+isEvenNumber(10);
+isEvenNumber(31);
 
 // Task 4:
 function movieTicketPrice(age) {
@@ -87,29 +93,31 @@ function temperatureConversion(temp) {
 temperatureConversion(6);
 
 // Task 6:
-function electricityBillCalc(num) {
+function getElectricityBill(num) {
     if (typeof num !== "number" || isNaN(num))
         return console.log("Please enter a valid number.");
 
     if (num <= 0) return console.log("Please enter a number greater than 0.");
 
     let total = 0;
+    let tier1 = 50 * 1.678;
+    let tier2 = 50 * 1.734;
 
     if (num <= 50) {
         total = num * 1.678;
-    } else if (num > 50 && num <= 100) {
-        total += num * 1.734;
-    } else if (num > 100 && num <= 200) {
-        total += num * 2.014;
+    } else if (num <= 100) {
+        total = tier1 + (num - 50) * 1.734;
+    } else if (num <= 200) {
+        total = tier1 + tier2 + (num - 100) * 2.014;
     } else {
-        total += num * 2.536;
+        total = tier1 + tier2 + 100 * 2.014 + (num - 200) * 2.536;
     }
 
     return console.log(`Amount to pay ${Math.round(total)}VND`);
 }
 
-electricityBillCalc(0);
-electricityBillCalc(49);
-electricityBillCalc(80);
-electricityBillCalc(140);
-electricityBillCalc(500);
+getElectricityBill(0);
+getElectricityBill(49);
+getElectricityBill(80);
+getElectricityBill(140);
+getElectricityBill(500);
